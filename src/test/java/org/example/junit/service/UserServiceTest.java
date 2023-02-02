1bb800c795f11e48c8996fcb38bb9c551ef9751e
ivlevks <ivlevks@yandex.ru>
2023-02-02 12:32:24 +0300

    base

---
+++ src/test/java/org/example/junit/service/UserServiceTest.java
@@ -0,0 +1,15 @@
+package org.example.junit.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UserServiceTest {
+
+    @Test
+    void test() {
+        assertTrue(false);
+    }
+
+}
